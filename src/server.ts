97040ba5702799daa2fa49bdb317235e0f838085
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2022-06-01 10:56:49 -0300

    Creating a abstract error class

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-import express from 'express';
+import 'reflect-metadata';
+
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+
+import AppError from './errors/AppError';
 import { testConnection } from './database/connection';
 
 import routes from './routes';
@@ -8,6 +12,21 @@ const app = express();
 
 app.use(cors());
 app.use(routes);
+app.use(
+  (err: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: err.message,
+    });
+  },
+);
 
 try {
   testConnection();
