73f82810c678656f800d53e708063477fd202f91
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2022-06-02 20:48:17 -0300

    Adding some clausules on Create Session service

--- src/services/SessionServices/CreateSessionService.ts
+++ src/services/SessionServices/CreateSessionService.ts
@@ -1,5 +1,6 @@
-import { parseISO } from 'date-fns';
+import { isBefore, parseISO } from 'date-fns';
 import Session from 'models/Session';
+import AppError from '../../errors/AppError';
 import knex from '../../database/connection';
 
 interface IRequest {
@@ -16,8 +17,39 @@ export default {
     id_local,
     nr_valorInteira,
   }: IRequest): Promise<Session> {
+    const parsedDate = parseISO(dt_sessao);
+
+    if (isBefore(parsedDate, Date.now())) {
+      throw new AppError(
+        'Não é possível cadastrar uma sessão em uma data passada.',
+      );
+    }
+
+    const findSessionInTheSameDate = await knex<Session>('tb_sessoes').where(
+      'dt_sessao',
+      parsedDate,
+    );
+
+    if (findSessionInTheSameDate.length > 0) {
+      throw new AppError(
+        'Não é possível cadastrar duas sessões com a mesma data',
+      );
+    }
+
+    const findEvent = await knex('tb_eventos').where('id_evento', id_evento);
+
+    if (!findEvent.length) {
+      throw new AppError('Evento não encontrado.');
+    }
+
+    const findPlace = await knex('tb_local').where('id_local', id_local);
+
+    if (!findPlace.length) {
+      throw new AppError('Local não encontrado.');
+    }
+
     const sessionData = {
-      dt_sessao: parseISO(dt_sessao),
+      dt_sessao: parsedDate,
       id_local,
       id_evento,
       nr_valorInteira,
