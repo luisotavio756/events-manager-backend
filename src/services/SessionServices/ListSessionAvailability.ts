833d4f604cbe3eab3179d6ce4562cc5c1f58a38c
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2022-06-11 10:01:06 -0300

    Returning more info on session availability

--- src/services/SessionServices/ListSessionAvailability.ts
+++ src/services/SessionServices/ListSessionAvailability.ts
@@ -1,4 +1,6 @@
 import { ISeatAvailability, ITicket } from 'types';
+import Session from 'models/Session';
+import Place from 'models/Place';
 import { getSessionAvailability } from '../../utils/utils';
 import knex from '../../database/connection';
 
@@ -6,10 +8,24 @@ interface IRequest {
   session_id: number;
 }
 
+interface IQueryResult
+  extends Omit<Session, 'id_sessao'>,
+    Omit<Place, 'id_local'> {
+  id_sessao: number;
+}
+
+interface IResponse {
+  id_sessao: number;
+  seats: number;
+  availableSeats: number;
+  sailedSeats: number;
+  seatsAvailability: ISeatAvailability[];
+}
+
 export default {
-  async run({ session_id }: IRequest): Promise<ISeatAvailability[]> {
+  async run({ session_id }: IRequest): Promise<IResponse> {
     const session = await knex('tb_sessoes')
-      .select('tb_local.*', 'tb_sessoes.id_sessao')
+      .select<IQueryResult[]>('tb_local.*', 'tb_sessoes.id_sessao')
       .innerJoin('tb_local', 'tb_sessoes.id_local', 'tb_local.id_local')
       .where('tb_sessoes.id_sessao', session_id);
 
@@ -17,12 +33,18 @@ export default {
       .returning('*')
       .where('id_sessao', session_id);
 
-    const seatsTotal = session[0].nr_assentos;
+    const { id_sessao, nr_assentos } = session[0];
     const seatsAvailability = getSessionAvailability({
       sailedSeats,
-      seatsTotal,
+      seatsTotal: nr_assentos,
     });
 
-    return seatsAvailability;
+    return {
+      id_sessao,
+      seats: nr_assentos,
+      availableSeats: nr_assentos - sailedSeats.length,
+      sailedSeats: sailedSeats.length,
+      seatsAvailability,
+    };
   },
 };
