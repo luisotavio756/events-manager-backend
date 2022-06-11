9180263192df6fba54cce5073fa0dfa929389857
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2022-06-11 09:50:53 -0300

    Returning available tickets on Session list

--- src/services/SessionServices/ListSessionsService.ts
+++ src/services/SessionServices/ListSessionsService.ts
@@ -5,11 +5,39 @@ interface IRequest {
   event_id: number;
 }
 
+interface IQueryResult extends Session {
+  sailedTickets: string;
+  availableTickets: number;
+  nr_assentos: number;
+}
+
+interface IResponse extends Omit<IQueryResult, 'sailedTickets'> {
+  sailedTickets: number;
+  availableTickets: number;
+}
+
 export default {
-  async run({ event_id }: IRequest): Promise<Session[]> {
-    const sessions = await knex<Session>('tb_sessoes')
-      .select('*')
+  async run({ event_id }: IRequest): Promise<IResponse[]> {
+    const queryResult = await knex('tb_sessoes')
+      .select<IQueryResult[]>('tb_sessoes.*', 'tb_local.nr_assentos')
+      .count<IQueryResult[]>('tb_sessoes.id_sessao AS sailedTickets')
+      .leftJoin(
+        'tb_ingressos',
+        'tb_ingressos.id_sessao',
+        '=',
+        'tb_sessoes.id_sessao',
+      )
+      .leftJoin('tb_local', 'tb_local.id_local', '=', 'tb_sessoes.id_local')
+      .groupBy('tb_sessoes.id_sessao')
+      .groupBy('tb_local.nr_assentos')
       .where('id_evento', event_id);
+
+    const sessions = queryResult.map((session: IQueryResult) => ({
+      ...session,
+      availableTickets:
+        Number(session.nr_assentos) - Number(session.sailedTickets),
+      sailedTickets: Number(session.sailedTickets),
+    }));
 
     return sessions;
   },
