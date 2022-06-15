5749adc2deca49b3033605c524110ff634388f42
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2022-06-14 21:21:51 -0300

    Fixing createEvent date problems

--- src/services/EventServices/CreateEventService.ts
+++ src/services/EventServices/CreateEventService.ts
@@ -1,4 +1,5 @@
 import { isBefore, parseISO } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 
 import SessionDTO from '../../dtos/SessionDTO';
 import AppError from '../../errors/AppError';
@@ -32,16 +33,18 @@ export default {
 
     const { id_evento } = insertedIds[0] as any;
 
-    sessoes.forEach(session => {
+    sessoes.forEach((session, index) => {
       const parsedDate = parseISO(session.date);
+      const dateWithTimezone = utcToZonedTime(new Date(), 'America/Sao_Paulo');
+      const sessionsWithoutIt = [...sessoes].filter((_, i) => i !== index);
 
-      if (isBefore(parsedDate, Date.now())) {
+      if (isBefore(parsedDate, dateWithTimezone)) {
         throw new AppError(
           'Não é possível cadastrar uma sessão em uma data passada.',
         );
       }
 
-      if (sessoes.find(s => s.date === session.date)) {
+      if (sessionsWithoutIt.find((s, i) => s.date === session.date)) {
         throw new AppError(
           'Não é possível cadastrar duas sessões com a mesma data',
         );
