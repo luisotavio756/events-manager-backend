7b0bfb60d74f4a37d8ef0909586c5459681eaeb6
Luis Otávio Lima Caminha <luis.otavio2425@gmail.com>
2022-06-17 14:54:17 -0300

    Returning the sessoes from an event

--- src/services/EventServices/ListSingleEventService.ts
+++ src/services/EventServices/ListSingleEventService.ts
@@ -1,15 +1,27 @@
 import Event from 'models/Event';
+import Session from 'models/Session';
+import AppError from '../../errors/AppError';
 import knex from '../../database/connection';
 
 interface IRequest {
-    id: number;
+  id: number;
 }
 
 export default {
   async run({ id }: IRequest): Promise<Event[]> {
-    const events = await knex('tb_eventos').select('*').where('id_evento', id)
+    const [event] = await knex('tb_eventos').select('*').where('id_evento', id);
 
-    return events;
+    if (!event) {
+      throw new AppError('Evento não encontrado.', 404);
+    }
+
+    const sessions = await knex<Session>('tb_sessoes')
+      .select('*')
+      .where('id_evento', id);
+
+    return {
+      ...event,
+      sessoes: sessions,
+    };
   },
 };
-
