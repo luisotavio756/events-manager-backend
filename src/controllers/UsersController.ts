b93753271ad9e50bd64106a015a6949421936bdf
Mateus Fonseca <mateus.xfl@gmail.com>
2022-06-17 16:24:42 -0300

    Add password confirmation

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -11,18 +11,28 @@ export default {
   },
 
   async create(request: Request, response: Response): Promise<Response> {
-    const { name, cpf, sexo, email, telephone, password, birthDate } = request.body;
+    const {
+      name,
+      cpf,
+      sexo,
+      email,
+      telephone,
+      password,
+      repeatPassword,
+      birthDate,
+    } = request.body;
 
     const createUser = await createUsersService.run({
-        ds_nome: name,
-        ds_cpf: cpf,
-        ds_sexo: sexo,
-        ds_email: email,
-        ds_telefone: telephone,
-        ds_login: email,
-        ds_senha: password,
-        dt_nascimento: birthDate,
-        id_tipo_usuario: 3,
+      ds_nome: name,
+      ds_cpf: cpf,
+      ds_sexo: sexo,
+      ds_email: email,
+      ds_telefone: telephone,
+      ds_login: email,
+      ds_senha: password,
+      ds_senha_confirmacao: repeatPassword,
+      dt_nascimento: birthDate,
+      id_tipo_usuario: 3,
     });
 
     return response.json(createUser);
